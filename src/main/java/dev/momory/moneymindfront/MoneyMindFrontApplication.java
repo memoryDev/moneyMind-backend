e5b944049b312c0cbcc9d8b13ddb82cdbb319d3f
memoryDev <115910144+memoryDev@users.noreply.github.com>
2024-08-27 15:11:08 +0900

    Initial commit

---
+++ src/main/java/dev/momory/moneymindfront/MoneyMindFrontApplication.java
@@ -0,0 +1,13 @@
+package dev.momory.moneymindfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyMindFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyMindFrontApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/momory/moneymindfront/MoneyMindFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.momory.moneymindfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyMindFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
